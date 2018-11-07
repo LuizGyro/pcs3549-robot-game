523e2e8b5b5b7fd12f29455e95b8c00737161473
Arthur Vieira Barbosa <arthur.vb@gmail.com>
2018-11-07 17:41:35 -0200

    Fixed collision

--- character/character.gd
+++ character/character.gd
@@ -1,49 +1,62 @@
 extends Node2D
 
-const SPEED = 200
-
 export var mass = 50
+export var max_speed = 200
 export var player_name = "p1"
 
 onready var pushbox = $Torso/Pushbox
 onready var right_attack = $Torso/RightArm/Hitbox/CollisionShape2D
 
-var vel = 0
+var shielding
+var speed = 0
 
 var right_attack_duration = .2
 var right_attack_timer = 0
 
 func _ready():
-    pass
+	pass
 
 
 func _physics_process(delta):
-    if !right_attack.disabled:
-        right_attack_timer += delta
-        if right_attack_timer >= right_attack_duration:
-            right_attack.disabled = true
-            right_attack_timer = 0
-    
-    if Input.is_action_pressed(player_name + "_shield"):
-        vel = 0
-        return
-    
-    if Input.is_action_pressed(player_name + "_left"):
-        vel = -SPEED
-    elif Input.is_action_pressed(player_name + "_right"):
-        vel = SPEED
-    else:
-        vel = 0
-    
-    var seila = vel
-    
-    if pushbox.get_overlapping_areas():
-        var other = pushbox.get_overlapping_areas()[0].get_parent().get_parent()
-        seila = (vel * mass + other.vel * other.mass) / (mass + other.mass)
-        printt(player_name, vel)
-    
-    position.x += seila * delta
-    
-    if Input.is_action_just_pressed(player_name + "_attack") and right_attack.disabled:
-        right_attack.disabled = false
-    
+	if !right_attack.disabled:
+		right_attack_timer += delta
+	if right_attack_timer >= right_attack_duration:
+		right_attack.disabled = true
+		right_attack_timer = 0
+	
+	if Input.is_action_pressed(player_name + "_shield"):
+		shielding = true
+	
+	if Input.is_action_pressed(player_name + "_left") and not shielding:
+		speed = -max_speed
+	elif Input.is_action_pressed(player_name + "_right") and not shielding:
+		speed = max_speed
+	else:
+		speed = 0
+	
+	var final_speed = speed
+	
+	if pushbox.get_overlapping_areas():
+		var other = pushbox.get_overlapping_areas()[0].get_parent().get_parent() # Consertar essa gambiarra depois
+		var delta_x = other.position.x - position.x
+		
+		var v1
+		var v2
+		if delta_x > 0:
+			v1 = speed
+			v2 = other.speed
+		else:
+			v1 = other.speed
+			v2 = speed
+		
+		# Acoplar
+		if (v1 > 0 and v2 <= 0) or (v1 == 0 and v2 < 0) or\
+				(v1 > v2 and sign(v1) == sign(v2)):
+			final_speed = (speed * mass + other.speed * other.mass) / (mass + other.mass)
+	
+	position.x += final_speed * delta
+	
+	if Input.is_action_just_pressed(player_name + "_attack") and\
+			right_attack.disabled:
+		right_attack.disabled = false
+
